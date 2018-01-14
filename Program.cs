2844d3018b928e7ab72bf79f9b0dca4e9465fb43
Luis Valladares <keepercito@localhost.localdomain>
2018-01-14 17:25:19 -0600

    generando y corriendo seeds

--- Program.cs
+++ Program.cs
@@ -3,23 +3,32 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using HolaMundo.Models;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace HolaMundo
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
+namespace HolaMundo {
+    public class Program {
+        public static void Main (string[] args) {
+            using (var scope = BuildWebHost (args).Services.CreateScope ()) {
+                var services = scope.ServiceProvider;
+                try {
+                    //Requiere using HolaMundo.Models
+                    SeedData.Initialize (services);
+                } catch (System.Exception e) {
+                    var logger = services.GetRequiredService<ILogger<Program>> ();
+                    logger.LogError (e, "Ocurrio un error llenando la base de datos");
+                }
+            }
+            BuildWebHost (args).Run ();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+        public static IWebHost BuildWebHost (string[] args) =>
+            WebHost.CreateDefaultBuilder (args)
+            .UseStartup<Startup> ()
+            .Build ();
     }
 }
