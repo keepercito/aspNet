7d65c4ff3be824566ebcf6263460bc63de823f6a
Luis Valladares <keepercito@localhost.localdomain>
2018-01-15 11:56:30 -0600

    validacion backend y frontend

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,16 +1,29 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 
-namespace HolaMundo.Models
-{
-    public class Movie
-    {
-        public int ID { get; set;}
-        public string Title { get; set;}
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        public DateTime ReleaseDate { get; set;}
-        public string Genre { get; set;}
-        public decimal Price { get; set;}
-        public string Rating { get; set;}
+namespace HolaMundo.Models {
+    public class Movie {
+        public int ID { get; set; }
+
+        [StringLength (60, MinimumLength = 3)]
+        [Required]
+        public string Title { get; set; }
+
+        [DisplayFormat (DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression (@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Required]
+        [StringLength (30)]
+        public string Genre { get; set; }
+
+        [Range (1, 100)]
+        [DataType (DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [RegularExpression (@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Required]
+        [StringLength (30)]
+        public string Rating { get; set; }
     }
 }
